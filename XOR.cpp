f7e4583576d25b2a6a08015bf4b10e08cc5e8b26
Nagaraj <nnagarajece31@gmail.com>
2021-11-12 18:22:20 +0530

    TLE

--- XOR.cpp
+++ XOR.cpp
@@ -1,76 +1,32 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using namespace std;
 int done_count;
 int T;
 int MAX_X = 18;
 
-int process(std::vector<long long int> input_array, int i, int data_len, int answers[])
+int log(string log_msg)
 {
-    long long int x = 0;
-    while (true)
-    {
-
-        if (data_len == 1)
-        {
-            long long int bigInteger = input_array[0];
-            if (bigInteger == 0)
-            {
-                answers[i] = 0;
-                break;
-            }
-            else
-            {
-                answers[i] = -1;
-                break;
-            }
-        }
-        else
-        {
-            long long int xorValue = (input_array[0] + x) ^ (input_array[1] + (x));
-            for (int m = 2; m < data_len; m++)
-            {
-                xorValue = xorValue ^ (input_array[m] + (x));
-            }
-
-            if (xorValue == 0)
-            {
-                answers[i] = x;
-                break;
-            }
-        }
-        x = x + 1;
-        if (x == (MAX_X))
-        {
-            answers[i] = -1;
-            break;
-        }
-    }
-
-    done_count++;
-
-    if (done_count == T)
-    {
-        for (int i; i < answers.size(); i++)
-        {
-            cout << answers[i];
-        }
-    }
+    std::ofstream outfile;
+    outfile.open("logs.txt", std::ios_base::app);
+    outfile << log_msg + "\n";
     return 0;
 }
 
 int main()
 {
     std::cin >> T;
-    vector<long long int> answer(T);
+    std::vector<long long int> answer(T);
 
     for (int i = 0; i < T; i++)
     {
         int N;
         std::cin >> N;
-        vector<long long int> input(N);
+        std::vector<long long int> input;
         long long int inp;
         for (int j = 0; j < N; j++)
         {
@@ -78,13 +34,16 @@ int main()
             input.push_back(inp);
         }
 
-        std::thread thread_object(process, input, i, N, answer);
-        thread_object.join();
-        /* 
-        long long int x = 0;
+        log("input values: ");
+        for (int j = 0; j < N; j++)
+        {
+            log(to_string(input[j]));
+        }
+
+        log("Processing index" + to_string(i));
+        int x = 0;
         while (true)
         {
-
             if (N == 1)
             {
                 long long int bigInteger = input[0];
@@ -101,14 +60,27 @@ int main()
             }
             else
             {
-                long long int xorValue = (input[0] + x) ^ (input[1] + (x));
+                long long int xor_value = (input[0] + x) ^ (input[1] + (x));
+                log("data_len value: " + to_string(N));
+
+                log(to_string((input[0] + x)));
+                log(to_string((input[1] + x)));
+                log(to_string((input[2] + x)));
+
+                log("(input[0] + x) ^ (input[1] + x) ^ (input[2] + x)): ");
+                log(to_string((input[0] + x) ^ (input[1] + x) ^ (input[2] + x)));
+
+                log(to_string(xor_value));
+
                 for (int m = 2; m < N; m++)
                 {
-                    xorValue = xorValue ^ (input[m] + (x));
+                    xor_value = xor_value ^ (input[m] + (x));
                 }
 
-                if (xorValue == 0)
+                log("xor value: " + to_string(xor_value));
+                if (xor_value == 0)
                 {
+                    log("xor value is 0, x value: " + to_string(x));
                     answer[i] = x;
                     break;
                 }
@@ -119,11 +91,16 @@ int main()
                 answer[i] = -1;
                 break;
             }
-        } */
-    }
+        }
 
-    /* for (int k = 0; k < T; k++)
-    {
-        std::cout << answer[k] << endl;
-    } */
+        done_count++;
+        if (done_count == T)
+        {
+            for (int k = 0; k < T; k++)
+            {
+                std::cout << answer[k] << endl;
+            }
+        }
+    }
+    return 0;
 }
